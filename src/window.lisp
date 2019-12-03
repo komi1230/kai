c6bf79053ed4e2a7befba5654846c10bc4ac01f9
Yusuke Kominami <yukke.konan@gmail.com>
2019-12-03 23:51:35 +0900

    added functions to plot scales

--- src/window.lisp
+++ src/window.lisp
@@ -28,7 +28,8 @@
                 :vertex
                 :with-primitive)
   (:export :make-base-window
-           :setup-base-window))
+           :setup-base-window
+           :plot))
 (in-package #:kai.window)
 
 
@@ -70,25 +71,76 @@
      (gl:ortho ,0 ,1 ,0 ,1 ,-1 ,1)))
 
 
-;; Draw input and frame
-(defmacro plot ()
+;; Draw input data and frame
+(defmacro make-figure ()
   `(defmethod glut:display ((w base-window))
      (gl:clear :color-buffer)
      
      ;; Draw frame
      (gl:color ,0 ,0 ,0)
      (gl:with-primitive :line-strip  ; bottom
-       (gl:vertex ,0.10 ,0.15 ,0)
-       (gl:vertex ,0.90 ,0.15 ,0))
+       (gl:vertex ,0.13 ,0.10 ,0)
+       (gl:vertex ,0.93 ,0.10 ,0))
      (gl:with-primitive :line-strip  ; top
-       (gl:vertex ,0.10 ,0.85 ,0)
-       (gl:vertex ,0.90 ,0.85 ,0))
+       (gl:vertex ,0.13 ,0.90 ,0)
+       (gl:vertex ,0.93 ,0.90 ,0))
      (gl:with-primitive :line-strip  ; left
-       (gl:vertex ,0.10 ,0.15 ,0)
-       (gl:vertex ,0.10 ,0.85 ,0))
+       (gl:vertex ,0.13 ,0.10 ,0)
+       (gl:vertex ,0.13 ,0.90 ,0))
      (gl:with-primitive :line-strip  ; right
-       (gl:vertex ,0.90 ,0.15 ,0)
-       (gl:vertex ,0.90 ,0.85 ,0))
+       (gl:vertex ,0.93 ,0.10 ,0)
+       (gl:vertex ,0.93 ,0.90 ,0))
+
+     ;; Draw Scale
+     ;; x-axis
+     (gl:color ,0 ,0 ,0)
+     (gl:with-primitive :line-strip     
+       (gl:vertex ,0.155 ,0.10 ,0)
+       (gl:vertex ,0.155 ,0.09 ,0))
+     (gl:with-primitive :line-strip 
+       (gl:vertex ,0.305 ,0.10 ,0)
+       (gl:vertex ,0.305 ,0.09 ,0))
+     (gl:with-primitive :line-strip 
+       (gl:vertex ,0.455 ,0.10 ,0)
+       (gl:vertex ,0.455 ,0.09 ,0))
+     (gl:with-primitive :line-strip
+       (gl:vertex ,0.605 ,0.10 ,0)
+       (gl:vertex ,0.605 ,0.09 ,0))
+     (gl:with-primitive :line-strip
+       (gl:vertex ,0.755 ,0.10 ,0)
+       (gl:vertex ,0.755 ,0.09 ,0))
+     (gl:with-primitive :line-strip
+       (gl:vertex ,0.905 ,0.10 ,0)
+       (gl:vertex ,0.905 ,0.09 ,0))
+     ;; y-axis
+     (gl:with-primitive :line-strip 
+       (gl:vertex ,0.12 ,0.12 ,0)
+       (gl:vertex ,0.13 ,0.12 ,0))
+     (gl:with-primitive :line-strip 
+       (gl:vertex ,0.12 ,0.215 ,0)
+       (gl:vertex ,0.13 ,0.215 ,0))
+     (gl:with-primitive :line-strip 
+       (gl:vertex ,0.12 ,0.31 ,0)
+       (gl:vertex ,0.13 ,0.31 ,0))
+     (gl:with-primitive :line-strip 
+       (gl:vertex ,0.12 ,0.405 ,0)
+       (gl:vertex ,0.13 ,0.405 ,0))
+     (gl:with-primitive :line-strip 
+       (gl:vertex ,0.12 ,0.50 ,0)
+       (gl:vertex ,0.13 ,0.50 ,0))
+     (gl:with-primitive :line-strip 
+       (gl:vertex ,0.12 ,0.595 ,0)
+       (gl:vertex ,0.13 ,0.595 ,0))
+     (gl:with-primitive :line-strip 
+       (gl:vertex ,0.12 ,0.69 ,0)
+       (gl:vertex ,0.13 ,0.69 ,0))
+     (gl:with-primitive :line-strip 
+       (gl:vertex ,0.12 ,0.785 ,0)
+       (gl:vertex ,0.13 ,0.785 ,0))
+     (gl:with-primitive :line-strip 
+       (gl:vertex ,0.12 ,0.88 ,0)
+       (gl:vertex ,0.13 ,0.88 ,0))
+     
 
      ;; FIXME: Draw input data.
      
@@ -99,9 +151,9 @@
 
 
 
-;; TEST FUNCTION !!!
+;;----------------- For Debug ---------------------
 (defun main ()
   (make-base-window *title* *width* *height*)
   (setup-base-window)
-  (plot-frame)
+  (make-figure)
   (glut:display-window (make-instance 'base-window)))
