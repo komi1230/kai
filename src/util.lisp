52acac5491b0654cd9dba611c6a5ad4ad4e224a8
Yusuke Kominami <komi@Yusuke.local>
2019-12-04 13:07:50 +0900

    added function to find min and max value from the input data

--- src/util.lisp
+++ src/util.lisp
@@ -69,13 +69,16 @@
 ;;; Like the above function (type and shape check), we will
 ;;; make some functions for each shape of the input data.
 ;;;
-;;; Each finding function returns cons: (MIN . MAX)
+;;; Each finding function returns cons:
+;;;     Simple => (MIN . MAX)
+;;;     Nested => ((X-MIN Y-MIN) . (X-MAX Y-MAX))
+;;;                  or ((X-MIN Y-MIN Z-MIN) . (X-MAX Y-MAX Z-MAX))
 
 ;; Case 1)
 (defun find-min-max-simple-lst (data)
   (labels ((f (min-value max-value lst)
              (if (null lst)
-                 (cons min-value max-value)
+                 (list min-value max-value)
                  (if (< max-value (car lst))
                      (f min-value (car lst) (cdr lst))
                      (if (< (car lst) min-value)
@@ -93,6 +96,79 @@
              (setq min-value (aref data i))
              (if (< max-value (aref data i))
                  (setq max-value (aref data i)))))
-    (cons min-value max-value)))
+    (list min-value max-value)))
 
 ;; Case 3)
+(defun find-min-max-simple-complex-lst (data)
+  (find-min-max-nested-lst (mapcar #'(lambda (x) (list (realpart x)
+                                                       (imagpart x)))
+                                   data)))
+
+;; Case 4)
+(defun find-min-max-nested-complex-vector (data)
+  (find-min-max-nested-lst (coerce (map 'vector
+                                        #'(lambda (x) (list (realpart x)
+                                                            (imagpart x)))
+                                        data)
+                                   'list)))
+
+;; Case 5)
+(defun find-min-max-nested-lst (data)
+  (labels ((replace-small (lst1 lst2)
+             (if (null lst1)
+                 nil
+                 (if (< (car lst1) (car lst2))
+                     (cons (car lst1)
+                           (replace-small (cdr lst1) (cdr lst2)))
+                     (cons (car lst2)
+                           (replace-small (cdr lst1) (cdr lst2))))))
+           (replace-big (lst1 lst2)
+             (if (null lst1)
+                 nil
+                 (if (< (car lst1) (car lst2))
+                     (cons (car lst2)
+                           (replace-big (cdr lst1) (cdr lst2)))
+                     (cons (car lst1)
+                           (replace-big (cdr lst1) (cdr lst2))))))
+           (get-big-pair (init lst)
+             (if (null lst)
+                 init
+                 (get-big-pair (replace-big init (car lst))
+                               (cdr lst))))
+           (get-small-pair (init lst)
+             (if (null lst)
+                 init
+                 (get-small-pair (replace-small init (car lst))
+                                 (cdr lst)))))
+    (cons (get-small-pair (car data) data)
+          (get-big-pair (car data) data))))
+
+;; Case 6)
+(defun find-min-max-nested-vector (data)
+  (let ((init-small (coerce (aref data 0) 'list))
+        (init-big (coerce (aref data 0) 'list)))
+    (loop for ith from 0 below (length data) do
+         (loop for idx from 0 below (length init-small) do
+              (let ((target-elem (aref (aref data ith) idx)))
+                (if (< target-elem (nth idx init-small))
+                    (setf (nth idx init-small) target-elem)
+                    (if (< (nth idx init-big) target-elem)
+                        (setf (nth idx init-big) target-elem))))))
+    (cons (coerce init-small 'list)
+          (coerce init-big 'list))))
+
+;; Summarize functions to find min max value.
+(defun find-min-max (data)
+  (if (listp data)
+      (if (listp (car data))
+          (find-min-max-nested-lst data)
+          (if (complexp (car data))
+              (find-min-max-simple-complex-lst data)
+              (find-min-max-simple-lst data)))
+      (if (vectorp data)
+          (if (vectorp (aref data 0))
+              (find-min-max-nested-vector data)
+              (if (complexp (aref data 0))
+                  (find-min-max-nested-complex-vector data)
+                  (find-min-max-simple-vector data)))
+          (error "Invalid input data: ~a~&" data))))
