c059ed14d9a95b8fe9f8d3c277f72e237dd342d9
Yusuke Kominami <komi@Yusuke.local>
2020-03-15 17:14:18 +0900

    enabled to generate JSON

--- src/interface.lisp
+++ src/interface.lisp
@@ -16,20 +16,57 @@
 (in-package #:kai.interface)
 
 
+
+;;;; Input style converter
+;;;
+;;; When getting input data, we accept variable length args.
+;;; We cannot realize to accept one or two args with some options by
+;;; standard style, so we papare such a function to convert args.
+
+(defun convert-data (&rest data)
+  (let ((x (car data))
+        (y (cadr data)))
+    (if (or (consp x)        ; check first data
+            (vectorp x))
+        (if (or (consp y)    ; check second data  
+                (vectorp y))
+            data
+            `((quote ,(loop for i below (length x) collect i))
+              (quote ,x)
+              ,@(cdr data)))
+        (error "Invalid input"))))
+
+
 ;;;; Scatter and Line
 ;;;
 ;;; This covers scatter and line plotting and their options.
 
-;; 2D
-(defun plot (data0 &optional data1 (mode "markers") (name ""))
-  (let ((len (length data0))
-        (x-axis (loop for i below len collect i)))
-    (if data1
-        (data-to-json x-axis
-                      data0
-                      :mode mode
-                      :name name)
-        (data-to-json data0
-                      data1
-                      :mode mode
-                      :name name))))
+;; 2D scatter
+(defun scatter (&rest data)
+  (eval `(-scatter ,@(apply #'convert-data data))))
+
+(defun -scatter (data0
+                 data1
+                 &key
+                   (type "scatter")
+                   (mode "markers")
+                   (name "")
+                   (text '())
+                   (error-x '())
+                   (error-y '())
+                   (fill "")
+                   (fillcolor "")
+                   (line '())
+                   (marker '()))
+  (scatter-to-json :data0 data0
+                   :data1 data1
+                   :type type
+                   :mode mode
+                   :name name
+                   :text text
+                   :error-x error-x
+                   :error-y error-y
+                   :fill fill
+                   :fillcolor fillcolor
+                   :line line
+                   :marker marker))
