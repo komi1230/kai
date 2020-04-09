c2cb7fb923629e92c0acb0c24594239d566530e3
Yusuke Kominami <yukke.konan@gmail.com>
2020-04-09 20:37:29 +0900

    modified a little

--- test/main.lisp
+++ test/main.lisp
@@ -1,16 +1,14 @@
 (defpackage kai-test
   (:use :cl
-        :kai
-        :rove))
+        :kai))
 (in-package :kai-test)
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :kai)' in your Lisp.
 
-(ql:quickload :rove)
 
 (rove:deftest test-target-kai
   (rove:testing "Initial setup for state"
     (rove:ok (equal kai:*state* '())))
 
-  (testing "Initial setup for style"
+  (rove:testing "Initial setup for style"
     (rove:ok (equal kai:*style "{}"))))
