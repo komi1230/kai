6f6066cbafba327a173199a85dc813102266ee91
SANO Masatoshi <snmsts@gmail.com>
2020-04-11 00:55:54 +0800

    remove side effect by loading kai

--- src/converter.lisp
+++ src/converter.lisp
@@ -40,66 +40,65 @@
 ;;; When accepted some inputs, we convert the data to JSON data at first
 ;;; because we design kai to be able to switch backends easily.
 
-
-;; Jonathan options
-(setf jonathan:*false-value* :false)
-(setf jonathan:*null-value* :null)
-(setf jonathan:*empty-array-value* :empty-array)
-(setf jonathan:*empty-object-value* :empty-object)
-
+(defun to-json (param)
+  (let ((jonathan:*false-value* :false)
+        (jonathan:*null-value* :null)
+        (jonathan:*empty-array-value* :empty-array)
+        (jonathan:*empty-object-value* :empty-object))
+    (jonathan:to-json param)))
 
 ;; Generate
 (defun data-to-json (&key
-                       (data0 :null)
-                       (data1 :null)
-                       (data2 '())
-                       (type "scatter")
-                       (mode "")
-                       (name "")
-                       (text '())
-                       (error-x '())
-                       (error-y '())
-                       (fill "")
-                       (fillcolor "")
-                       (line '())
-                       (marker '())
-                       (value '())
-                       (label '())
-                       (parents '())
-                       (boxmean "")
-                       (boxpoints "")
-                       (showscale :false)
-                       (colorscale '())
-                       (contours '())
-                       (autocontour :false))
-  (jonathan:to-json `(:|x| ,data0
-                      :|y| ,data1
-                      :|z| ,data2 
-                      :|type| ,type
-                      :|mode| ,mode
-                      :|name| ,name
-                      :|text| ,text
-                      :|error_x| ,error-x
-                      :|error_y| ,error-y
-                      :|fill| ,fill
-                      :|fillcolor| ,fillcolor
-                      :|line| ,line
-                      :|marker| ,marker
-                      :|values| ,value
-                      :|labels| ,label
-                      :|parents| ,parents
-                      :|boxmean| ,boxmean
-                      :|boxpoints| ,boxpoints
-                      :|showscale| ,showscale
-                      :|colorscale| ,colorscale
-                      :|contours| ,contours
-                      :|autocontour| ,autocontour)))
+                     (data0 :null)
+                     (data1 :null)
+                     (data2 '())
+                     (type "scatter")
+                     (mode "")
+                     (name "")
+                     (text '())
+                     (error-x '())
+                     (error-y '())
+                     (fill "")
+                     (fillcolor "")
+                     (line '())
+                     (marker '())
+                     (value '())
+                     (label '())
+                     (parents '())
+                     (boxmean "")
+                     (boxpoints "")
+                     (showscale :false)
+                     (colorscale '())
+                     (contours '())
+                     (autocontour :false))
+  (to-json `(:|x| ,data0
+             :|y| ,data1
+             :|z| ,data2
+             :|type| ,type
+             :|mode| ,mode
+             :|name| ,name
+             :|text| ,text
+             :|error_x| ,error-x
+             :|error_y| ,error-y
+             :|fill| ,fill
+             :|fillcolor| ,fillcolor
+             :|line| ,line
+             :|marker| ,marker
+             :|values| ,value
+             :|labels| ,label
+             :|parents| ,parents
+             :|boxmean| ,boxmean
+             :|boxpoints| ,boxpoints
+             :|showscale| ,showscale
+             :|colorscale| ,colorscale
+             :|contours| ,contours
+             :|autocontour| ,autocontour)))
 
 
 (defun style-to-json (&key
-                        title
-                        xaxis
-                        yaxis)
-  (jonathan:to-json `(:|title| ,title
-                      :|xaxis| ,xaxis
-                      :|yaxis| ,yaxis)))
+                      title
+                      xaxis
+                      yaxis)
+  (to-json `(:|title| ,title
+             :|xaxis| ,xaxis
+             :|yaxis| ,yaxis)))
