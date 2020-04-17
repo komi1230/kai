969e34d1c08b6df10cbfcdcb32c45814a18b3dac
Yusuke Kominami <yukke.konan@gmail.com>
2020-04-17 22:31:44 +0900

    by polarcellarray

--- src/gr/GR.lisp
+++ src/gr/GR.lisp
@@ -12,7 +12,9 @@
 (defpackage :kai.GR.GR
   (:use :cl)
   (:import-from :kai.converter
-                :make-kai-cache))
+                :make-kai-cache)
+  (:import-from :kai.util
+                :flatten))
 (in-package :kai.GR.GR)
 
 
@@ -53,10 +55,7 @@
     ;; Load shared objects
     (cffi:load-foreign-library
      (merge-pathnames (format nil "lib/~A" libGR)
-                      (make-kai-cache "gr")))
-
-    ;; Default float number is as Double
-    (setf *read-default-float-format* 'double-float)))
+                      (make-kai-cache "gr")))))
 
 
 
@@ -183,7 +182,7 @@ workstation_id :
 
 Deactivate the specified workstation.
 
-`workstation_id` :
+workstation_id :
     A workstation identifier.
 |#
 
@@ -205,6 +204,317 @@ Deactivate the specified workstation.
 
 
 
+#|
+    Memory allocation for Array
+
+When drawing graph, we have to provide data as array pointer
+to drawing function.
+
+lst : 
+    A list containing data to plot
+|#
+
+(defun data-alloc (lst type)
+  (cffi:foreign-alloc type
+                      :initial-contents
+                      (mapcar #'(lambda (x)
+                                  (case type
+                                    (:int (coerce x 'integer))
+                                    (:float (coerce x 'single-float))
+                                    (:double (coerce x 'double-float))))
+                              lst)))
+
+(defun string-alloc (str)
+  (cffi:foreign-string-alloc str))
+
+
+#|
+    polyline
+
+Draw a polyline using the current line attributes, starting from the
+first data point and ending at the last data point.
+
+x :
+    A list containing the X coordinates
+
+y :
+    A list containing the Y coordinates
+
+|#
+
+(cffi:defcfun ("gr_polyline" gr-polyline) :void
+  (n :int)
+  (x (:pointer :double))
+  (y (:pointer :double)))
+
+(defun polyline (x y)
+  (if (= (length x)
+         (length y))
+      (gr-polyline (length x)
+                   (data-alloc x :double)
+                   (data-alloc y :double))
+      (error "Invalid input data")))
+
+
+#|
+    polymarker
+
+Draw marker symbols centered at the given data points.
+
+x :
+    A list containing the X coordinates
+
+y :
+    A list containing the Y coordinates
+
+|#
+
+(cffi:defcfun ("gr_polymarker" gr-polymarker) :void
+  (n :int)
+  (x (:pointer :double))
+  (y (:pointer :double)))
+
+(defun polymarker (x y)
+  (if (= (length x)
+         (length y))
+      (gr-polymarker (length x)
+                     (data-alloc x :double)
+                     (data-alloc y :double))
+      (error "Invalid input data")))
+
+
+#|
+    text(x::Real, y::Real, string)
+
+Draw a text at position `x`, `y` using the current text attributes.
+
+x :
+    The X coordinate of starting position of the text string
+
+y :
+    The Y coordinate of starting position of the text string
+
+strin` :
+    The text to be drawn
+
+The values for x and y are in normalized device coordinates.
+The attributes that control the appearance of text are text font and precision,
+character expansion factor, character spacing, text color index, character
+height, character up vector, text path and text alignment.
+|#
+
+(cffi:defcfun ("gr_text" gr-text) :void
+  (x :double)
+  (y :double)
+  (str (:pointer :char)))
+
+(defun text (x y str)
+  (gr-text (coerce x 'double-float)
+           (coerce y 'double-float)
+           (string-alloc str)))
+
+(cffi:defcfun ("gr_inqtext" gr-inqtext) :void
+  (x :double)
+  (y :double)
+  (str (:pointer :char))
+  (tbx (:pointer :double))
+  (tby (:pointer :double)))
+
+(defun inqtext (x y str)
+  (gr-inqtext (coerce x 'double-float)
+              (coerce y 'double-float)
+              (string-alloc str)
+              (data-alloc '(0 0 0 0) :double)
+              (data-alloc '(0 0 0 0) :double)))
+
+
+#|
+    fillarea(x, y)
+
+Allows you to specify a polygonal shape of an area to be filled.
+
+x :
+    A list containing the X coordinates
+y :
+    A list containing the Y coordinates
+
+The attributes that control the appearance of fill areas are fill area interior
+style, fill area style index and fill area color index.
+|#
+
+(cffi:defcfun ("gr_fillarea" gr-fillarea) :void
+  (n :int)
+  (x (:pointer :double))
+  (y (:pointer :double)))
+
+(defun fillarea (x y)
+  (if (= (length x)
+         (length y))
+      (gr-fillarea (length x)
+                   (data-alloc x :double)
+                   (data-alloc y :double))
+      (error "Invalid data inputs")))
+
+
+#|
+    cellarray(xmin::Real, xmax::Real, ymin::Real, ymax::Real, dimx::Int, dimy::Int, color)
+
+Display rasterlike images in a device-independent manner. The cell array
+function partitions a rectangle given by two corner points into DIMX X DIMY
+cells, each of them colored individually by the corresponding color index
+of the given cell array.
+
+xmin, ymin :
+    Lower left point of the rectangle
+
+xmax, ymax :
+    Upper right point of the rectangle
+
+dimx, dimy :
+    X and Y dimension of the color index array
+
+color :
+    Color index array
+
+The values for xmin, xmax, ymin and ymax are in world coordinates.
+|#
+
+(cffi:defcfun ("gr_cellarray" gr-cellarray) :void
+  (xmin :double)
+  (xmax :double)
+  (ymin :double)
+  (ymax :double)
+  (dimx :int)
+  (dimy :int)
+  (scol :int)
+  (srow :int)
+  (ncol :int)
+  (nrow :int)
+  (color (:pointer :int)))
+
+(defun cellarray (xmin xmax ymin ymax dimx dimy color)
+  (gr-cellarray xmin
+                xmax
+                ymin
+                ymax
+                dimx
+                dimy
+                1    ; scol
+                1    ; srow
+                dimx ; ncol
+                dimy ; nrow
+                (data-alloc (flatten color) :int)))
+
+
+#|
+    nonuniformcellarray(x, y, dimx::Int, dimy::Int, color)
+
+ Display a two dimensional color index array with nonuniform cell sizes.
+
+x, y :
+    X and Y coordinates of the cell edges
+
+dimx, dimy :
+    X and Y dimension of the color index array
+
+color :
+    Color index array
+
+The values for x and y are in world coordinates. x must contain dimx + 1 elements
+and y must contain dimy + 1 elements. The elements i and i+1 are respectively the edges
+of the i-th cell in X and Y direction.
+|#
+
+(cffi:defcfun ("gr_nonuniformcellarray" gr-nonuniformcellarray) :void
+  (x (:pointer :double))
+  (y (:pointer :double))
+  (dimx :int)
+  (dimy :int)
+  (scol :int)
+  (srow :int)
+  (ncol :int)
+  (nrow :int)
+  (color (:pointer :int)))
+
+(defun nonuniformcellarray (x y dimx dimy color)
+  (gr-nonuniformcellarray (data-alloc x :double)
+                          (data-alloc y :double)
+                          dimx
+                          dimy
+                          1
+                          1
+                          dimx
+                          dimy
+                          (data-alloc (flatten color) :int)))
+
+
+#|
+    polarcellarray(xorg::Real, yorg::Real, phimin::Real, phimax::Real, rmin::Real, rmax::Real, imphi::Int, dimr::Int, color)
+
+Display a two dimensional color index array mapped to a disk using polar
+coordinates.
+
+xorg :
+    X coordinate of the disk center in world coordinates
+
+yorg :
+    Y coordinate of the disk center in world coordinates
+
+phimin :
+    start angle of the disk sector in degrees
+
+phimax :
+    end angle of the disk sector in degrees
+
+rmin :
+    inner radius of the punctured disk in world coordinates
+
+rmax :
+    outer radius of the punctured disk in world coordinates
+
+dimiphi, dimr :
+    Phi (X) and iR (Y) dimension of the color index array
+
+color :
+    Color index array
+
+The two dimensional color index array is mapped to the resulting image by
+interpreting the X-axis of the array as the angle and the Y-axis as the raidus.
+The center point of the resulting disk is located at xorg, yorg and the
+radius of the disk is `rmax`.
+|#
+
+(cffi:defcfun ("gr_polarcellarray" gr-polarcellarray) :void
+  (xorg :double)
+  (yorg :double)
+  (phimin :double)
+  (phimax :double)
+  (rmin :double)
+  (rmax :double)
+  (dimphi :int)
+  (dimr :int)
+  (scol :int)
+  (srow :int)
+  (ncol :int)
+  (nrow :int)
+  (color (:pointer :int)))
+
+(defun polarcellarray (xorg yorg phimin phimax rmin rmax dimphi dimr color)
+  (gr-polarcellarray xorg
+                     yorg
+                     phimin
+                     phimax
+                     rmin
+                     rmax
+                     dimphi
+                     dimr
+                     1
+                     1
+                     dimphi
+                     dimr
+                     (data-alloc (flatten color) :int)))
+
+
 
 ;;;; sample codes
 
@@ -219,10 +529,7 @@ Deactivate the specified workstation.
                       :initial-contents
                       (list 0.3d0 0.5d0 0.4d0 0.2d0 0.6d0 0.7d0)))
 
-(cffi:defcfun ("gr_polyline" polyline) :void
-  (n :int)
-  (x :pointer)
-  (y :pointer))
+
 
 (cffi:defcfun ("gr_tick" tick) :double
   (a :double)
