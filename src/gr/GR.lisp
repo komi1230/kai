a4c036415193ee4227b664caadfb9cb4518659ed
Yusuke Kominami <yukke.konan@gmail.com>
2020-04-24 21:46:03 +0900

    almost all functions in gr.h finished

--- src/gr/GR.lisp
+++ src/gr/GR.lisp
@@ -3955,3 +3955,317 @@ The available transformation types are:
                    h)
     (free x-data
           y-data)))
+
+
+(cffi:defcfun ("gr_panzoom" gr-panzoom) :void
+  (x :double)
+  (y :double)
+  (xzoom :double)
+  (yzoom :double)
+  (xmin (:pointer :double))
+  (xmax (:pointer :ouuble))
+  (ymin (:pointer :double))
+  (ymax (:pointer :double)))
+
+(defun panzoom (x y zoom)
+  (let ((xmin-data (data-alloc '(0) :double))
+        (xmax-data (data-alloc '(0) :double))
+        (ymin-data (data-alloc '(0) :double))
+        (ymax-data (data-alloc '(0) :double)))
+    (gr-panzoom (coerce x 'double-float)
+                (coerce y 'double-float)
+                (coerce zoom 'double-float)
+                (coerce zoom 'double-float)
+                xmin-data
+                xmax-data
+                ymin-data
+                ymax-data)
+    (free xmin-data
+          xmax-data
+          ymin-data
+          ymax-data)))
+
+
+#|
+    path(x, y, codes)
+
+Draw paths using the given vertices and path codes.
+
+`x` :
+    A list containing the X coordinates
+`y` :
+    A list containing the Y coordinates
+`codes` :
+    A list containing the path codes
+
+The values for `x` and `y` are in world coordinates.
+The `codes` describe several path primitives that can be used to create compound paths.
+The following path codes are recognized:
+
++----------+---------------------------------+-------------------+-------------------+
+|   Code   |          Description            |         X         |         Y         |
++----------+---------------------------------+-------------------+-------------------+
+|     M, m | move                            | x                 | y                 |
++----------+---------------------------------+-------------------+-------------------+
+|     L, l | line                            | x                 | y                 |
++----------+---------------------------------+-------------------+-------------------+
+|     Q, q | quadratic Bezier                | x1, x2            | y1, y2            |
++----------+---------------------------------+-------------------+-------------------+
+|     C, c | cubic Bezier                    | x1, x2, x3        | y1, y2, y3        |
++----------+---------------------------------+-------------------+-------------------+
+|     A, a | arc                             | rx, a1, reserved  | ry, a2, reserved  |
++----------+---------------------------------+-------------------+-------------------+
+|        Z | close path                      |                   |                   |
++----------+---------------------------------+-------------------+-------------------+
+|        S | stroke                          |                   |                   |
++----------+---------------------------------+-------------------+-------------------+
+|        s | close path and stroke           |                   |                   |
++----------+---------------------------------+-------------------+-------------------+
+|        f | close path and fill             |                   |                   |
++----------+---------------------------------+-------------------+-------------------+
+|        F | close path, fill and stroke     |                   |                   |
++----------+---------------------------------+-------------------+-------------------+
+
+- Move: `M`, `m`
+   Moves the current position to (`x`, `y`). The new position is either absolute (`M`) or relative to the current
+   position (`m`). The initial position of :code:`path` is (0, 0).
+
+   Example:
+
+   >>> (path '(0.5 -0.1) '(0.2 0.1) "Mm")
+
+   The first move command in this example moves the current position to the absolute coordinates (0.5, 0.2). The
+   second move to performs a movement by (-0.1, 0.1) relative to the current position resulting in the point
+   (0.4, 0.3).
+
+- Line: `L`, `l`
+   Draws a line from the current position to the given position (`x`, `y`). The end point of the line is either
+   absolute (`L`) or relative to the current position (`l`). The current position is set to the end point of the
+   line.
+
+   Example:
+
+   >>> (path '(0.1 0.5 0.0) '(0.1 0.1 0.2) "MLlS")
+
+   The first line to command draws a straight line from the current position (0.1, 0.1) to the absolute position
+   (0.5, 0.1) resulting in a horizontal line. The second line to command draws a vertical line relative to the
+   current position resulting in the end point (0.5, 0.3).
+
+- Quadratic Bezier curve: `Q`, `q`
+   Draws a quadratic bezier curve from the current position to the end point (`x2`, `y2`) using (`x1`, `y1`) as the
+   control point. Both points are either absolute (`Q`) or relative to the current position (`q`). The current
+ 
+   position is set to the end point of the bezier curve.
+
+   Example:
+
+   >>> (path '(0.1 0.3 0.5 0.2 0.4) '(0.1 0.2 0.1 0.1 0.0) "MQqS")
+
+   This example will generate two bezier curves whose start and end points are each located at y=0.1. As the control
+   points are horizontally in the middle of each bezier curve with a higher y value both curves are symmetrical
+   and bend slightly upwards in the middle. The current position is set to (0.9, 0.1) at the end.
+
+- Cubic Bezier curve: `C`, `c`
+   Draws a cubic bezier curve from the current position to the end point (`x3`, `y3`) using (`x1`, `y1`) and
+   (`x2`, `y2`) as the control points. All three points are either absolute (`C`) or relative to the current position
+   (`c`). The current position is set to the end point of the bezier curve.
+
+   Example:
+
+   >>> (path '(0.1 0.2 0.3 0.4 0.1 0.2 0.3)
+   ...       '(0.1 0.2 0.0 0.1 0.1 -0.1 0.0)
+   ...       "MCcS")
+
+   This example will generate two bezier curves whose start and end points are each located at y=0.1. As the control
+   points are equally spaced along the x-axis and the first is above and the second is below the start and end
+   points this creates a wave-like shape for both bezier curves. The current position is set to (0.8, 0.1) at the
+   end.
+
+- Ellipctical arc: `A`, `a`
+   Draws an elliptical arc starting at the current position. The major axis of the ellipse is aligned with the x-axis
+   and the minor axis is aligned with the y-axis of the plot. `rx` and `ry` are the ellipses radii along the major
+   and minor axis. `a1` and `a2` define the start and end angle of the arc in radians. The current position is set
+   to the end point of the arc. If `a2` is greater than `a1` the arc is drawn counter-clockwise, otherwise it is
+   drawn clockwise. The `a` and `A` commands draw the same arc. The third coordinates of the `x` and `y` array are
+   ignored and reserved for future use.
+
+   Examples:
+
+   >>> (path '(0.1 0.2 (/ -3.14159 2) 0.0) '(0.1 0.4 (/ 3.14159 2) 0.0) "MAS")
+
+   This example draws an arc starting at (0.1, 0.1). As the start angle -pi/2 is smaller than the end angle pi/2 the
+   arc is drawn counter-clockwise. In this case the right half of an ellipse with an x radius of 0.2 and a y radius
+   of 0.4 is shown. Therefore the current position is set to (0.1, 0.9) at the end.
+   >>> path([0.1, 0.2, 3.14159 / 2, 0.0], [0.9, 0.4, -3.14159 / 2, 0.0], "MAS")
+   This examples draws the same arc as the previous one. The only difference is that the starting point is now at
+   (0.1, 0.9) and the start angle pi/2 is greater than the end angle -pi/2 so that the ellipse arc is drawn
+   clockwise. Therefore the current position is set to (0.1, 0.1) at the end.
+
+- Close path: `Z`
+   Closes the current path by connecting the current position to the target position of the last move command
+   (`m` or `M`) with a straight line. If no move to was performed in this path it connects the current position to
+   (0, 0). When the path is stroked this line will also be drawn.
+
+- Stroke path: `S`, `s`
+   Strokes the path with the current border width and border color (set with :code:`gr.setborderwidth` and
+   :code:`gr.setbordercolorind`). In case of `s` the path is closed beforehand, which is equivalent to `ZS`.
+
+- Fill path: `F`, `f`
+   Fills the current path using the even-odd-rule using the current fill color. Filling a path implicitly closes
+   the path. The fill color can be set using :code:`gr.setfillcolorind`. In case of `F` the path is also
+   stroked using the current border width and color afterwards.
+
+|#
+
+(cffi:defcfun ("gr_path" gr-path) :void
+  (n :int)
+  (x (:pointer :double))
+  (y (:pointer :double))
+  (codes (:pointer :char)))
+
+(defun path (x y codes)
+  (assert (= (length x)
+             (length y)))
+  (let ((x-data (data-alloc x :double))
+        (y-data (data-alloc y :double))
+        (code-data (string-alloc codes)))
+    (gr-path (length x)
+             x-data
+             y-data
+             code-data)
+    (free x-data y-data)
+    (string-free code-data)))
+
+
+#|
+    setborderwidth(width::Real)
+
+Define the border width of subsequent path output primitives.
+
+`width` :
+    The border width scale factor
+
+|#
+
+(cffi:defcfun ("gr_setborderwidth" gr-setborderwidth) :void
+  (width :double))
+
+(defun setborderwidth (width)
+  (gr-setborderwidth (coerce width 'double-float)))
+
+
+#|
+    setbordercolorind(color::Int)
+
+Define the color of subsequent path output primitives.
+
+`color` :
+    The border color index (COLOR < 1256)
+
+|#
+
+(cffi:defcfun ("gr_setbordercolorind" gr-setbordercolorind) :void
+  (color :int))
+
+(defun setbordercolorind (color)
+  (gr-setbordercolorind color))
+
+
+#|
+    setprojectiontype(int flag)
+
+Set the projection type with this flag.
+
+flag :
+    projection type
+
+The available options are:
+
++---------------------------+---+--------------+
+|GR_PROJECTION_DEFAULT      |  0|default       |
++---------------------------+---+--------------+
+|GR_PROJECTION_ORTHOGRAPHIC |  1|orthographic  |
++---------------------------+---+--------------+
+|GR_PROJECTION_PERSPECTIVE  |  2|perspective    |
++---------------------------+---+--------------+
+
+|#
+
+(cffi:defcfun ("gr_setprojectiontype" gr-setprojectiontype) :void
+  (flag :int))
+
+(defun setprojectiontype (flag)
+  (gr-setprojectiontype flag))
+
+
+(cffi:defcfun ("gr_setperspectiveprojection" gr-setperspectiveprojection) :void
+  (near-plane :double)
+  (far-plane :double)
+  (fov :double))
+
+(defun setperspectiveprojection (near far fov)
+  (gr-setperspectiveprojection (coerce near 'double-float)
+                               (coerce far 'double-float)
+                               (coerce fov 'double-float)))
+
+
+(cffi:defcfun ("gr_settransformationparameters" gr-settransformationparameters) :void
+  (camera-pos-x :double)
+  (camera-pos-y :double)
+  (camera-pos-z :double)
+  (up-x :double)
+  (up-y :double)
+  (up-z :double)
+  (focus-point-x :double)
+  (focus-point-y :double)
+  (focus-point-z :double))
+
+(defun settransformationparameters (camera-x camera-y camera-z
+                                    up-x up-y up-z
+                                    focus-x focus-y focus-z)
+  (gr-settransformationparameters (coerce camera-x 'double-float)
+                                  (coerce camera-y 'double-float)
+                                  (coerce camera-z 'double-float)
+                                  (coerce up-x 'double-float)
+                                  (coerce up-y 'double-float)
+                                  (coerce up-z 'double-float)
+                                  (coerce focus-x 'double-float)
+                                  (coerce focus-y 'double-float)
+                                  (coerce focus-z 'double-float)))
+
+
+(cffi:defcfun ("gr_setorthographicprojection" gr-setorthographicprojection) :void
+  (left :double)
+  (right :double)
+  (bottom :double)
+  (top :double)
+  (near :double)
+  (far :double))
+
+(defun setorthographicprojection (left right bottom top
+                                  near far)
+  (gr-setorthographicprojection (coerce left 'double-float)
+                                (coerce right 'double-float)
+                                (coerce bottom 'double-float)
+                                (coerce top 'double-float)
+                                (coerce near 'double-float)
+                                (coerce far 'double-float)))
+
+
+(cffi:defcfun ("gr_setwindow3d" gr-setwindow3d) :void
+  (xmin :double)
+  (xmax :double)
+  (ymin :double)
+  (ymax :double)
+  (zmin :double)
+  (zmax :double))
+
+(defun setwindow3d (xmin xmax ymin ymax zmin zmax)
+  (gr-setwindow3d (coerce xmin 'double-float)
+                  (coerce xmax 'double-float)
+                  (coerce ymin 'double-float)
+                  (coerce ymax 'double-float)
+                  (coerce zmin 'double-float)
+                  (coerce zmax 'double-float)))
+
+
