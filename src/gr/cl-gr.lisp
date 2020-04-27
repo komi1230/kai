3c60329082ab965953424110b2ad8ff21a8e99a1
Yusuke Kominami <yukke.konan@gmail.com>
2020-04-27 13:56:06 +0900

    initialized cl-gr.lisp

---
+++ src/gr/cl-gr.lisp
@@ -0,0 +1,8 @@
+;;;; cl-gr.lisp --- An interface for GR
+;;;
+;;; This code has been placed in the Public Domain.  All warranties
+;;; are disclaimed.
+;;;
+;;; This file is a bridge between API of GR and Kai.
+
+
